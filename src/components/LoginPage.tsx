10f00edaf5f410e1c669ea1f9914792625709348
StudentPP1 <mishamakytonin@gmail.com>
2024-11-25 20:45:21 +0200

    add styles to login page & show error messages

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -4,7 +4,9 @@ import { UserContext, UserState } from '../utils/context';
 
 function LoginPage() {
   const [isLogin, setIsLogin] = useState<boolean>(false);
-  const {user, setUser} = useContext<UserState>(UserContext)
+  const [error, setError] = useState<string | null>(null)
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+  const { user, setUser } = useContext<UserState>(UserContext)
 
   const login = async (event: any) => {
     console.log("login")
@@ -12,17 +14,31 @@ function LoginPage() {
     const username = event.target['username'].value;
     const password = event.target['password'].value;
 
-    await fetch("http://localhost:8080/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*",
-        "Access-Control-Allow-Headers": "*",
-      },
-      credentials: "include",
-      body: JSON.stringify({ username: username, password: password }),
-    }).then(async () => {getSession()})
+    if (username == "" || password == "") {
+      setError("fields should not be empty")
+    }
+    else {
+      await fetch("http://localhost:8080/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "*",
+          "Access-Control-Allow-Headers": "*",
+        },
+        credentials: "include",
+        body: JSON.stringify({ username: username, password: password }),
+      }).then(res => {
+        if (!res.ok) {
+          return res.text().then(text => {
+            setError(text)
+          })
+        }
+        else {
+          getSession()
+        }
+      })
+    }
   }
 
   const register = async (event: any) => {
@@ -31,18 +47,31 @@ function LoginPage() {
     const name = event.target['name'].value;
     const username = event.target['username'].value;
     const password = event.target['password'].value;
-
-    await fetch("http://localhost:8080/auth/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "http://localhost:3000",
-        "Access-Control-Allow-Methods": "*",
-        "Access-Control-Allow-Headers": "*",
-      },
-      credentials: "include",
-      body: JSON.stringify({ name: name, username: username, password: password }),
-    }).then(async () => {getSession()})
+    if (username == "" || password == "" || name == "") {
+      setError("fields should not be empty")
+    }
+    else {
+      await fetch("http://localhost:8080/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "http://localhost:3000",
+          "Access-Control-Allow-Methods": "*",
+          "Access-Control-Allow-Headers": "*",
+        },
+        credentials: "include",
+        body: JSON.stringify({ name: name, username: username, password: password }),
+      }).then(res => {
+        if (!res.ok) {
+          return res.text().then(text => {
+            setError(text)
+          })
+        }
+        else {
+          getSession()
+        }
+      })
+    }
   }
 
   const getSession = async () => {
@@ -59,8 +88,14 @@ function LoginPage() {
     if (result.ok) {
       const json = await result.json();
       console.log(json);
+      setError(null)
       setUser(json)
     }
+  }
+
+  if (error) {
+    window.alert(error)
+    setError(null)
   }
 
   return (
@@ -71,47 +106,47 @@ function LoginPage() {
         }>
           <div className="form__group field">
             <input
-              type="input"
+              type="text"
               className="form__field"
-              name="name" id='name' 
-              style={{ display: isLogin ? "none" : "block" }}
-            />
+              placeholder="name" name="name" id='name'
+              style={{ display: isLogin ? "none" : "block" }} />
             <label
               className="form__label"
-              style={{ display: isLogin ? "none" : "block" }}
-            >
-              name
-            </label>
+              style={{ display: isLogin ? "none" : "block" }}>name</label>
           </div>
 
           <div className="form__group field">
-            <input
-              type="input"
-              className="form__field"
-              name="username" id='username' required
-            />
+            <input 
+            type="text" 
+            className="form__field" 
+            placeholder="username" name="username" id='username' required />
             <label className="form__label">username</label>
           </div>
 
           <div className="form__group field">
-            <input
-              type="password"
-              className="form__field"
-              name="password" id='password' required
-            />
+            <input 
+            type={showPassword ? "text" : "password" }
+            className="form__field" 
+            placeholder="password" name="password" id='password' required />
             <label className="form__label">password</label>
+            <div className='show-password-button' onClick={() => setShowPassword(!showPassword)}>
+            {showPassword ? "hide" : "show" }
+            </div>
           </div>
 
           <div className='login-page__form-submit-button'>
-            <button type="submit">
-              {isLogin ? "login" : "register"}
+            <button type="submit" className='form-submit-button'>
+              {isLogin ? "Login" : "Register"}
             </button>
           </div>
 
           <div className='login-page__form-change-button'>
-            <button type="button" onClick={() => { setIsLogin(!isLogin) }}>
-              {isLogin ? "Don't have account? Register now" : "Already have account? Login now"}
-            </button>
+            <span>
+            {isLogin ? "Don't have account? " : "Already have account? "}
+            </span>
+            <a className='change-button' type="button" onClick={() => { setIsLogin(!isLogin) }}>
+              {isLogin ? "Register now" : "Login now"}
+            </a>
           </div>
 
         </form>
